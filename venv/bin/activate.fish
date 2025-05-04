c6d9d1fc8b823b5b18be20da980f8f118fa3e460
Fifi-0321 <yw6945@nyu.edu>
2025-05-04 03:42:34 -0400

    UI update and create_order page

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/wuyufei/meal-sharing-app/venv"
+set -gx VIRTUAL_ENV "/Users/wuyufei/Documents/MealMates_Proj/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
