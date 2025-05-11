86dcb718e34fc66fdac21fc76d9e6fa785aa71d0
agnes-hon <ph2224@nyu.edu>
2025-05-10 21:57:53 -0400

    Add files via upload
    Refactor API /api/restaurants_by_zip/<zipcode> for tighter radius + cuisine filtering
    Integrate delivery time estimation (via OpenRouteService, Google Maps, etc.)
    Added ‘cuisine_type’ to the Restaurant data and disabled the grade type

---
+++ import_res.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from models import engine, Restaurant, UserPreference
+from app import app, db
+from sqlalchemy import text
+
+# Read restaurant data from remote MySQL
+with engine.connect() as conn:
+    df_res = pd.read_sql(text("SELECT name, address, latitude, longitude, score, cuisine FROM restaurants"), conn)
+
+    print("Restaurants table columns:", list(df_res.columns))
+    print(df_res.head())
+    
+with app.app_context():
+    db.create_all()
+    Restaurant.query.delete()
+    db.session.commit()
+    for _, row in df_res.iterrows():
+        cuisine = row.get("Cuisine", "")
+        if pd.notna(cuisine):
+            cuisine = str(cuisine).lower().replace("/", ",").replace("  ", " ").strip()
+        else:
+            cuisine = None
+            
+        restaurant = Restaurant(
+            name=row["name"],
+            address=row["address"],
+            latitude=row.get("latitude"),
+            longitude=row.get("longitude"),
+            score=row.get("SCORE"),
+            cuisine_type=cuisine
+        )
+        db.session.add(restaurant)
+        
+    db.session.commit()
+
+print(f"Imported {len(df_res)} restaurants into local db")
+print("Restaurant table columns:", list(df_res.columns))
+
+
+
+#read user pref
+# with engine.connect() as conn:
+#     df_pref = pd.read_sql(text("SELECT * FROM user_preferences_DB"), conn)
+# with app.app_context():
+#     for _, row in df_pref.iterrows():
+#         preference = UserPreference(
+#             school_id=row["School ID"],
+#             location=row.get("Where do you live?", ""),
+#             eat_time=row.get("When do you usually eat?", ""),
+#             delivery_freq=row.get("How often do you order delivery?", ""),
+#             dietary_restrictions=row.get("Do you have any dietary restrictions?", ""),
+#             cuisine_asian=row.get("Cuisine_Asian", 0),
+#             cuisine_italian=row.get("Cuisine_Italian", 0),
+#             cuisine_mexican=row.get("Cuisine_Mexican", 0),
+#             cuisine_middle_eastern=row.get("Cuisine_MiddleEastern", 0),
+#             # cuisine_american=row.get("Cuisine_American", 0),
+#             # cuisine_fastcasual=row.get("Cuisine_FastCasual", 0),
+#             spice_level=row.get("What is your spice tolerance level?", "0"),
+#             budget_level=row.get("What is your meal budget per person?", "0")
+#         )
+#         db.session.add(preference)
+#     db.session.commit()
+
+# print(f"Imported {len(df_pref)} user preferences into local Flask DB.")
+
